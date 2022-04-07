60023f50ab5f5ee79e19e6ac1df9a1760ccc85c7
Buddya <tsebekov94@gmail.com>
2022-04-07 17:49:48 +0300

    "refactoring (concurrent collections was used)"

--- servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -1,33 +1,41 @@
 package ru.netology.repository;
 
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
-  private Map<Long, Post> posts = new HashMap<>();
-  private long id = 1;
+    private final Map<Long, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicLong id = new AtomicLong(1L);
 
-  public List<Post> all() {
-    return new ArrayList<>(posts.values());
-  }
+    public List<Post> all() {
+        return new ArrayList<>(posts.values());
+    }
 
-  public Optional<Post> getById(long id) {
-    return Optional.ofNullable(posts.get(id));
-  }
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.get(id));
+    }
 
-  public synchronized Post save(Post post) {
-    Post newPost =  new Post(id, post.getContent());
-    posts.put(id, newPost);
-    id++;
-    return newPost;
-  }
+    public Post save(Post post) {
+        if (post.getId() == 0L) {
+            post.setId(id.getAndIncrement());
+            posts.put(id.get(), new Post(id.get(), post.getContent()));
+            return post;
+        } else {
+            final Post oldPost = getById(post.getId()).orElseThrow(NotFoundException::new);
+            oldPost.setContent(post.getContent());
+            return oldPost;
+        }
+    }
 
-  public synchronized void removeById(long id) {
-    posts.remove(id);
-  }
+    public void removeById(long id) {
+        posts.remove(id);
+    }
 }
